def3b922dcdbba0a48f88162aa813b33b635ffae
Michal Stolarczyk <michal.stolarczyk0@gmail.com>
2018-09-02 23:04:00 +0200

    moved discriminator and generator definitions to notebook, new dataset - lfw

--- src/gan.py
+++ src/gan.py
@@ -1,22 +1,17 @@
 import numpy as np
-from keras.layers import (
-    Dense, Conv2D, Conv2DTranspose, Flatten, MaxPool2D, Reshape
-)
 from keras.models import Input, Model
 from keras.optimizers import Adam
 
 
 class GAN:
-    def __init__(self,
-                 input_dim=2,
-                 noise_dim=2,
-                 discriminator_units=(10, 10),
-                 generator_units=(10, 10)):
+    def __init__(self, create_generator_func, create_discriminator_func,
+                 input_dim=2, noise_dim=2):
+
         self.input_dim = input_dim
         self.noise_dim = noise_dim
-        self.discriminator = self._create_discriminator(discriminator_units)
+        self.discriminator = create_discriminator_func(input_dim)
         self._compile_models()
-        self.generator = self._create_generator(generator_units)
+        self.generator = create_generator_func(noise_dim)
         self.discriminator.trainable = False
         self.combined_model = self._create_combined_model()
         self._compile_combined_model()
@@ -45,49 +40,15 @@ class GAN:
 
                 g_loss = self.combined_model.train_on_batch(
                     np.concatenate([noise, new_noise]),
-                    np.ones(batch_size))
+                    0.95 * np.ones(batch_size))
                 generator_losses.append(g_loss[0])
                 discriminator_losses.append(d_loss[0])
                 generator_accs.append(g_loss[1])
                 discriminator_accs.append(d_loss[1])
             print(np.mean(generator_losses), ",", np.mean(discriminator_losses),
-                  end=", ")
-            print(np.mean(generator_accs), ",", np.mean(discriminator_accs))
-
-    def _create_discriminator(self, hidden_units):
-        inputs = Input(shape=self.input_dim)
-        # hidden_state = Dense(hidden_units[0], activation="relu")(inputs)
-        # for n_units in hidden_units[1:]:
-        #     hidden_state = Dense(n_units, activation="relu")(hidden_state)
-        # outputs = Dense(1, activation="sigmoid")(hidden_state)
-        hidden_state = Conv2D(hidden_units[0], (3,3), padding="same", activation="relu")(inputs)
-        for n_units in hidden_units[1:]:
-            # hidden_state = BatchNormalization()(hidden_state)
-            hidden_state = Conv2D(n_units, (3, 3), padding="same", activation="relu")(hidden_state)
-            hidden_state = MaxPool2D(2)(hidden_state)
-        hidden_state = Flatten()(hidden_state)
-        hidden_state = Dense(20)(hidden_state)
-        outputs = Dense(1, activation="sigmoid")(hidden_state)
-        return Model(inputs, outputs)
-
-    def _create_generator(self, hidden_units):
-        inputs = Input(shape=(self.noise_dim,))
-        hidden_state = Dense(8*8*hidden_units[0], activation="relu")(inputs)
-
-        hidden_state = Reshape((8, 8, hidden_units[0]))(hidden_state)
-        for n_units in hidden_units[1:]:
-            hidden_state = Conv2DTranspose(
-                n_units, (3, 3), strides=2, padding="same", activation="relu"
-            )(hidden_state)
-            # hidden_state = BatchNormalization()(hidden_state)
-            hidden_state = Conv2D(
-                n_units, (3, 3), padding="same", activation="relu"
-            )(hidden_state)
-            hidden_state = Conv2D(
-                n_units, (3, 3), padding="same", activation="relu"
-            )(hidden_state)
-        outputs = Conv2D(1, (5, 5), padding="valid")(hidden_state)
-        return Model(inputs, outputs)
+                  end=", ", sep='')
+            print(np.mean(generator_accs), ",", np.mean(discriminator_accs),
+                  sep="")
 
     def _create_combined_model(self):
         noise = Input(shape=(self.noise_dim,))
